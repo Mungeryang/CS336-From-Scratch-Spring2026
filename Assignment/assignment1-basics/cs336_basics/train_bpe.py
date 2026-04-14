f90f8d17651f99871d10aab6f51fc60ffbd326df
mungermiao <1875706036@qq.com>
2026-04-14 21:47:40 +0800

    upload exp1

--- Assignment/assignment1-basics/cs336_basics/train_bpe.py
+++ Assignment/assignment1-basics/cs336_basics/train_bpe.py
@@ -1,1 +1,19 @@
+import torch.nn as nn 
+import torch.nn.functional as F
 
+
+
+def load_sample_file(file_name:str,special_token:str):
+    pass
+
+
+def init_vocab():
+    pass
+
+
+
+
+
+class BPETokenizer:
+    def __init__(self) -> None:
+        pass
