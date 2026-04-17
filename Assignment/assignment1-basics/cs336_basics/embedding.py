ce20d029502a555a05619fec3b41661cbf2ea969
mungermiao <1875706036@qq.com>
2026-04-17 21:25:45 +0800

    upload transformers

---
+++ Assignment/assignment1-basics/cs336_basics/embedding.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+class Embedding(nn.Module):
+    def __init__(self, num_embeddings, enbedding_dim, device=None, dtype=None):
+        pass
+        
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        pass
---
+++ Assignment/assignment1-basics/cs336_basics/linear.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+
+class Linear(nn.Module):
+    
+    def __init__(self, in_features, out_features, device=None, dtype=None):
+        pass
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        
+        pass
