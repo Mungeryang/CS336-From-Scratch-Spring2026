ac8d931f5279b1226c4fd36c17496655153fa54d
mungermiao <1875706036@qq.com>
2026-07-06 22:51:13 +0800

    upload config.py

--- handonllm/LoRA-SFT/config.py
+++ handonllm/LoRA-SFT/config.py
@@ -1,2 +1,20 @@
-# LoRA核心配置文件
+# LoRA微调 核心配置文件
+# 学习资料参考： 李宏毅老师 - 机器学习
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import peft
+
+max_seq_length = 2048
+
+model, tokenizer = AutoModelForCausalLM.from_pretrained(
+    model_name = " ",
+    dtype = torch.bfloat16,
+    max_seq_length = max_seq_length,
+)
+
+
+
+
+
 
